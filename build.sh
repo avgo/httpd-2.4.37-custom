d6417acb8695179285c140fb05b08f997a533786
Anton Goncharov <gavmail1985@yandex.ru>
2019-04-12 18:45:10 +0300

    Base testing files and script.

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+script_rp="$(realpath "${0}")"          || exit 1
+script_dir="$(dirname "${script_rp}")"  || exit 1
+
+# config_rp="${script_dir}/build.sh.conf"
+
+apache_port=8050
+apache_prefix="${script_dir}-root"
+
+load_config() {
+	source "$config_rp" || exit 1
+	if test x"$prefix" = x; then
+		echo error: prefix must be specified. >&2
+		exit 1
+	fi
+}
+
+main() {
+	test1 "$@"
+}
+
+test1() {
+	cd "${script_dir}" || return 1
+	make distclean
+	rm -rf "${apache_prefix}"
+	./configure --prefix="${apache_prefix}" &&
+	make &&
+	make install
+
+	# ServerRoot "%APACHE_SERVER_ROOT%"
+	# Listen %APACHE_PORT%
+	# PidFile %APACHE_PID%
+	# DocumentRoot %APACHE_DEBIAN%
+
+	local cf
+
+	mkdir -v "${apache_prefix}"/{etc{,/httpd{,/conf,/logs}},var{,/run,/www{,/html}}}
+
+	touch "${apache_prefix}/etc/httpd/mime.types"
+
+	mv -v "${apache_prefix}/modules" "${apache_prefix}/etc/httpd"
+
+	sed	-e "s@%APACHE_DEBIAN%@${apache_prefix}/var/www/html@g" \
+		-e "s@%APACHE_PID%@${apache_prefix}/var/run/apache.pid@g" \
+		-e "s@%APACHE_PORT%@${apache_port}@g" \
+		-e "s@%APACHE_SERVER_ROOT%@${apache_prefix}/etc/httpd@g" \
+		"httpd.conf.in" > "${apache_prefix}/etc/httpd/conf/httpd.conf"
+}
+
+main "$@"
